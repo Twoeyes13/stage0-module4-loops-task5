2b6c09de989a0acd9bd751eeb6e4807501edf879
Nursultan Dulatov <80988817+Twoeyes13@users.noreply.github.com>
2023-02-20 18:08:17 +0600

    All task done!

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (j == Math.ceil(sideLength / 2) + 1) System.out.print("8");
+                else if (i == Math.ceil(sideLength / 2) + 1) System.out.print("8");
+                    else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,45 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        boolean isEven = height % 2 == 0;
 
+        //First half of hourglass
+        int start = 0;
+        int end = height+1;
+
+        for (int i = 1; i <= Math.round(height / 2); i++) {
+            for (int j = 1; j <= height; j++) {
+                if (j > start && j < end){
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            start++;
+            end--;
+            System.out.println("");
+        }
+
+        if (!isEven) {
+            for (int i = 1; i <= height; i++) {
+                if (i == Math.ceil(height / 2) + 1) System.out.print("8");
+                    else System.out.print(" ");
+            }
+            System.out.println();
+        }
+        start--;
+        end++;
+        for (int i = 1; i <= Math.round(height / 2); i++) {
+            for (int j = 1; j <= height; j++) {
+                if (j > start && j < end){
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            start--;
+            end++;
+            System.out.println("");
+        }
     }
 }
